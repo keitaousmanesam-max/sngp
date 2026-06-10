0c6a996d98d850ee57b37c5c583d0618064a6579
samkeita <samkeita@gmail.com>
2026-06-10 13:46:27 +0200

    Force HTTPS en production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +18,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
