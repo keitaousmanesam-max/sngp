eced1ec6f1c5b3b0e84645bfc4da05a68cf6f56c
samkeita <samkeita@gmail.com>
2026-06-10 15:05:42 +0200

    Fix port dynamique Railway

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,7 @@ php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+sed -i "s/listen 8080/listen \/g" /etc/nginx/sites-available/default
 php-fpm -D
 sleep 2
 nginx -g 'daemon off;'
